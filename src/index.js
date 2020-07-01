921beeced3ecfec5c33f0e8bcb53d5efa8a443d1
fofotevo <estevamcabral1308@gmail.com>
2020-06-30 21:24:26 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Button from "@material-ui/core/Button";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-function App() {
-  return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  );
-}
-
-ReactDOM.render(<App />, document.querySelector("#app"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
